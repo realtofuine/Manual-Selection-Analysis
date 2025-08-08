43fa6a7b3f5a4f03e503d8807b981821c6aa0f85
realtofuine <73127480+realtofuine@users.noreply.github.com>
2025-08-08 13:34:54 -0400

    update ffmpeg

--- make_video.sh
+++ make_video.sh
@@ -4,6 +4,6 @@ eval "$(conda shell.bash hook)"
 # Now activate your env
 conda activate manualenv
 cd $1
-
+FFMPEG_BIN=$(python -c "import imageio_ffmpeg as ff; print(ff.get_ffmpeg_exe())")
 # ffmpeg -threads 4 -framerate 30 -pattern_type glob -i "IMG_*.JPG" -vf format=yuv420p -c:v libx264 -preset ultrafast -movflags +faststart $2
-ffmpeg -threads 4 -framerate 30 -pattern_type glob -i "IMG_*.JPG" -vf format=yuv420p -c:v libx264 -preset ultrafast -crf 18 -movflags +faststart $2
+"$FFMPEG_BIN" -threads 4 -framerate 30 -pattern_type glob -i "IMG_*.JPG" -vf format=yuv420p -c:v libx264 -preset ultrafast -crf 18 -movflags +faststart $2
